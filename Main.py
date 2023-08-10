37506d818e46efe1a44f421a8cfdb2025c84591a
Aqusely <kirillnalivajko96@gmail.com>
2023-08-10 16:48:02 +0500

    Нуждается в дороботке

--- Main.py
+++ Main.py
@@ -1,22 +1,34 @@
 import random 
 
-user_trys = []     #список пользовательских попыток
+user_tries = []
 random_number = random.randint(1, 100)    # Генерация случайного числа
-count = 0  #Переменная счета кол-ва попыток 
+
+def hints(our_number):
+    if random_number > our_number:
+        return f'Guess number > {our_number}'
+    else:
+        return f'Guess number < {our_number}'
+
 
 def start_game(answer):
     if answer == 'yes':
-        return game_logic()
+        return game_logic(int(input('Enter your number: ')))
     else:
         print('Bye')
 
 
-def game_logic():
-    tries  = input('Enter your number: ')
-    while int(tries) != random_number:
-        user_trys.append(tries)
-        tries  = input('Enter your number: ')
-    print('You win')
+def game_logic(guess):
+
+    count = 0   #Число попыток 
+    while guess != random_number:
+        guess = input('Enter your number: ')
+        if guess == 'hints' or guess == 'h':
+            print(hints(user_tries[-1]))
+        else:
+            count += 1
+            user_tries.append(guess)
+
+    
 
 print(random_number)
 start_game(input('Are you ready to play my game?: '))

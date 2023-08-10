4043da8bb10d973ac3f463598ebdda75ac90bd26
Aqusely <kirillnalivajko96@gmail.com>
2023-08-10 13:31:24 +0500

    Первый этап  игры
    Написана простейшая логика игры без подсказок, очков  счета и т.д.

--- Main.py
+++ Main.py
@@ -1,8 +1,17 @@
-random_number = 50
+import random 
 
+random_number = random.randint(1, 100)
+
+def start_game(answer):
+    if answer == 'yes':
+        game_logic(int(input("Enter your number: ")))
+    else:
+        print('Bye')
+    
 def game_logic(count):
     while count != random_number:
         count = int(input('Enter your number: '))
     print('You win')
 
-game_logic(int(input('Enter your number: ')))
+print(random_number)
+start_game(input('Are you ready to play my game?: '))
